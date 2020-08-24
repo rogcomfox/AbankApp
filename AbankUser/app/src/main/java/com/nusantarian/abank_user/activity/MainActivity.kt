5587b8375e656fe9a1c252467f7100fbb2eed495
rogcomfox <joeg.11021996@gmail.com>
2020-08-24 20:38:10 +0700

    add main fragment

--- AbankUser/app/src/main/java/com/nusantarian/abank_user/activity/MainActivity.kt
+++ AbankUser/app/src/main/java/com/nusantarian/abank_user/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package com.nusantarian.abank_user.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.nusantarian.abank_user.R
 import com.nusantarian.abank_user.databinding.ActivityMainBinding
+import com.nusantarian.abank_user.fragment.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentManager.OnBackStackChangedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,5 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.addOnBackStackChangedListener(this)
+        initMainFragment()
+    }
+
+    private fun initMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frame_main, MainFragment())
+            .commit()
+    }
+
+    override fun onBackStackChanged() {
+
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0)
+            supportFragmentManager.popBackStack()
+        else
+            super.onBackPressed()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        supportFragmentManager.popBackStack()
+        return true
     }
 }
