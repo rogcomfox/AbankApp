183ac3164a61f650654ae6997b008a520a6b7243
rogcomfox <joeg.11021996@gmail.com>
2020-10-05 10:53:07 +0700

    success show map

--- Abank-Gov/app/src/main/java/com/nusantarian/abank_gov/fragment/MainFragment.kt
+++ Abank-Gov/app/src/main/java/com/nusantarian/abank_gov/fragment/MainFragment.kt
@@ -1,17 +1,20 @@
 package com.nusantarian.abank_gov.fragment
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.FirebaseAuth
 import com.nusantarian.abank_gov.R
+import com.nusantarian.abank_gov.activity.LoginActivity
 import com.nusantarian.abank_gov.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(), OnMapReadyCallback {
@@ -19,6 +22,7 @@ class MainFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,10 +31,54 @@ class MainFragment : Fragment(), OnMapReadyCallback {
         // Inflate the layout for this fragment
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment =
-            activity?.supportFragmentManager?.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.app_name)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        setHasOptionsMenu(true)
+        auth = FirebaseAuth.getInstance()
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.map.onCreate(savedInstanceState)
+        binding.map.getMapAsync(this)
+        binding.map.onResume()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_log_out ->
+                raiseDialog()
+            R.id.nav_notifications ->
+                Toast.makeText(activity, "Under Construction", Toast.LENGTH_SHORT).show()
+            R.id.nav_message ->
+                Toast.makeText(activity, "Under Construction", Toast.LENGTH_SHORT).show()
+            R.id.nav_profile ->
+                Toast.makeText(activity, "Under Construction", Toast.LENGTH_SHORT).show()
+            R.id.nav_settings ->
+                Toast.makeText(activity, "Under Construction", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun raiseDialog() {
+        val builder = MaterialAlertDialogBuilder(activity!!)
+        builder.setTitle(R.string.nav_log_out)
+        builder.setMessage(R.string.text_message_log_out)
+        builder.setPositiveButton(R.string.text_yes) { _, _ ->
+            auth.signOut()
+            startActivity(Intent(activity, LoginActivity::class.java))
+            activity?.finish()
+        }
+        builder.setNegativeButton(R.string.text_no) { _, _ ->
+
+        }
+        builder.show()
     }
 
     /**
